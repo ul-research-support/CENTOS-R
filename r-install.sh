e9f0fac3b041f6238491af8b93863d9f0190cc54
William A. Mattingly <wamatt02@users.noreply.github.com>
2017-07-19 11:00:59 -0400

    Update r-install.sh

--- r-install.sh
+++ r-install.sh
@@ -7,17 +7,6 @@ gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 apt-get update -y
 apt-get install -y r-base
-sudo su - -c "R -e \"install.packages('shiny', repos = 'http://cran.rstudio.com/')\""
 apt-get -y install libcurl4-gnutls-dev libxml2-dev libssl-dev
 sudo su - -c "R -e \"install.packages('devtools', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('zoo', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('lubridate', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('qcc', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('Hmisc', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('ggplot2', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('xtable', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('redcapAPI', repos='http://cran.rstudio.com/')\""
-sudo su - -c "R -e \"install.packages('rmarkdown', repos='http://cran.rstudio.com/')\""
-sudo apt-get install -y gdebi-core
-sudo wget -O shiny-server.deb http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.5.3.838-amd64.deb
-sudo yes | gdebi shiny-server.deb
+sudo su - -c "R -e \"install.packages('tableone', repos='http://cran.rstudio.com/')\""
